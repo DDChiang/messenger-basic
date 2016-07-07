d1faca71bc4757cb84e14041ff3a8ec2f4ee912f
Deedee Chiang <deedeechiang@Deedee-Chiang-Macbook-Pro.local>
2016-07-06 22:31:38 -0700

    add postback event

--- index.js
+++ index.js
@@ -108,6 +108,11 @@ app.post('/webhook/', function(req, res) {
           }
           sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
       }
+      if (event.postback) {
+      	let text = JSON.stringify(event.postback)
+      	sendTextMessage(sender, "Postbakc received: "+text.substring(0,200), token)
+      	continue
+      }
   }
   res.sendStatus(200)
 }); 
