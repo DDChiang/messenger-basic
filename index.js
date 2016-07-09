22a11dc34cc25d89c1179b35af489c0ae056f107
Deedee Chiang <deedeechiang@Deedee-Chiang-Macbook-Pro.local>
2016-07-08 18:47:37 -0700

    clean up some code

--- index.js
+++ index.js
@@ -5,123 +5,116 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-var quiz = require('./quiz');
-var welcomeIndex = require('./welcomeIndex');
+const quiz = require('./quiz');
+const welcomeIndex = require('./welcomeIndex');
 
 function sendTextMessage(sender, text) {
-	let messageData = {text: text}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-        recipient: {id:sender},
-        message: messageData,
-    }
-	}, function(error, response, body) {
-		if (error) {
-        console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error)
-    }
-	});
+	let messageData = {
+		recipient: {id: sender},
+		message: {text: text}
+	}
+	callSendAPI(messageData);
 }
 
 function sendGenericMessage(sender, type) {
-	var messageData;
-
-	if (type==="generic") {
-		messageData = {
-			"attachment": {
-				"type": "template",
-				"payload": {
-					"template_type": "generic",
-					"elements": [{
-						"title": "First card",
-						"subtitle": "Element #1 of an hscroll",
-						"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-						"buttons": [{
-							"type": "web_url",
-							"url": "https://www.messenger.com",
-							"title": "web url"
-						}, {
-							"type": "postback",
-							"title": "Postback",
-							"payload": "Payload for first element in generic bubble"
-						},{
-							"type": "postback",
-							"title": "Quiz of Thing",
-							"payload": "quiz"
-						}] 
-					}, {
-						"title": "Second card",
-						"subtitle": "Element #2 of hscroll",
-						"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-						"buttons": [{
-							"type": "postback",
-	            "title": "Postback",
-	            "payload": "Payload for second element in a generic bubble",
-						}]
-					}]
-				}
-			}
-		}
-	}
-
-	if (type==="test") {
-		messageData = {
-			"attachment": {
-				"type": "template",
-				"payload": {
-					"template_type": "generic",
-					"elements": [
-						{
-							"title": "Test title",
-							"subtitle": "Test subtitle",
+	var messageContent, messageData;
+	switch(type) {
+		case 'generic': 
+			messageContent = {
+				"attachment": {
+					"type": "template",
+					"payload": {
+						"template_type": "generic",
+						"elements": [{
+							"title": "First card",
+							"subtitle": "Element #1 of an hscroll",
+							"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
 							"buttons": [{
-								"type": "postback",
-								"title": "Red",
-								"payload": "red"
+								"type": "web_url",
+								"url": "https://www.messenger.com",
+								"title": "web url"
 							}, {
 								"type": "postback",
-								"title": "Blue",
-								"payload": "blue"
+								"title": "Postback",
+								"payload": "Payload for first element in generic bubble"
 							},{
 								"type": "postback",
-								"title": "Cancel",
-								"payload": "cancel"
+								"title": "Quiz of Thing",
+								"payload": "quiz"
 							}] 
-						}, 
-						{
+						}, {
 							"title": "Second card",
 							"subtitle": "Element #2 of hscroll",
+							"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
 							"buttons": [{
 								"type": "postback",
 		            "title": "Postback",
 		            "payload": "Payload for second element in a generic bubble",
 							}]
-						}
-					]
+						}]
+					}
 				}
 			}
-		}
-	}
-
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token: token},
-		method: 'POST',
-		json: {
-			recipient: {id: sender},
-			message: messageData,
-		}
-	}, function(err, res, body) {
-			if (err) {
-				console.log('Error sending messages: ', err)
-			} else if (res.body.error) {
-				console.log('Error', res.body.error);
+		break	
+		case 'test': 
+			messageContent = {
+				"attachment": {
+					"type": "template",
+					"payload": {
+						"template_type": "generic",
+						"elements": [
+							{
+								"title": "Test title",
+								"subtitle": "Test subtitle",
+								"buttons": [{
+									"type": "postback",
+									"title": "Red",
+									"payload": "red"
+								}, {
+									"type": "postback",
+									"title": "Blue",
+									"payload": "blue"
+								},{
+									"type": "postback",
+									"title": "Cancel",
+									"payload": "cancel"
+								}] 
+							}, 
+							{
+								"title": "Second card",
+								"subtitle": "Element #2 of hscroll",
+								"buttons": [{
+									"type": "postback",
+			            "title": "Postback",
+			            "payload": "Payload for second element in a generic bubble",
+								}]
+							}
+						]
+					}
+				}
 			}
-	})
+		break
+	}
+	messageData = {
+		recipient: {id: sender},
+		message: messageContent
+	} 
+	callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: messageData
+	}, function(error, response, body) {
+		if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+	});
 }
 
 app.set('port', (process.env.PORT || 5000))
@@ -149,54 +142,40 @@ app.get('/webhook/', function (req, res) {
 app.post('/webhook/', function(req, res) {
 	let messaging_events = req.body.entry[0].messaging
   for (let i = 0; i < messaging_events.length; i++) {
-      let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
-      // user sends message
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
 
-      // if user sends media
-      if (event.message && event.message.attachments) {
-      	// for emoticons error message: '(#100) No matching user found',
-      	console.log('attachment');
-      	console.log(event.message);
-      	console.log(event.message.attachments[0].payload);
+    if (event.message && event.message.text) {
+      let text = event.message.text.toLowerCase()
+      switch (text) {
+      	case 'generic': 
+      		sendGenericMessage(sender, 'generic')
+      	break
+      	case 'quiz':
+       		// TODO: look for word combos or regex i.e. "i want to take test. can i take test? take quiz"
+      		sendTextMessage(sender, 'prompt quiz', token)
+      	break
+      	case 'test':
+        	sendGenericMessage(sender, 'test')
+      	break
+      	default:
+      		sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
       }
+    }
 
-      if (event.message && event.message.text) {
-          let text = event.message.text.toLowerCase()
-
-          if (text === 'generic') {
-          	sendGenericMessage(sender, 'generic')
-          	continue
-          }
-           // TODO: look for word combos or regex i.e. "i want to take test. can i take test? take quiz"
-          if (text === 'quiz') {
-          	sendTextMessage(sender, 'prompt quiz', token)
-          	continue
-          }
-
-          // Use to test structure
-          if (text === 'test') {
-          	sendGenericMessage(sender, 'test')
-          	continue
-          }
-          sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-      }
-      // we send payload back
-      if (event.postback) {
-      	let postbackVal = event.postback.payload.toLowerCase();
-
-      	if (postbackVal ==="quiz") {
-      		sendTextMessage(sender, "TODO: QUIZ " + quiz, token)
-      		continue
-      	}
-      	if (postbackVal ==="cancel") {
-      		sendTextMessage(sender, "cancel quiz", token)
-      		continue
-      	}
-      	// let text = JSON.stringify(event.postback)
-      	sendTextMessage(sender, "Postback received: "+JSON.stringify(event.postback), token)
-      	continue
-      }
+    if (event.postback) {
+    	let postbackVal = event.postback.payload.toLowerCase();
+    	switch (postbackVal) {
+    		case 'quiz': 
+    			sendTextMessage(sender, "TODO: QUIZ " + quiz, token)
+    		break
+    		case 'cancel':
+    			sendTextMessage(sender, "cancel quiz", token)
+    		break
+    		default: 
+    		sendTextMessage(sender, "Postback received: "+JSON.stringify(event.postback), token)
+    	}
+    }
   }
   res.sendStatus(200)
 }); 
--- quiz.js
+++ quiz.js
@@ -1,33 +1,12 @@
-var quiz = function() {
-	return `<div>
-		<script>
-				window.fbAsyncInit = function() {
-	      FB.init({
-	        appId: "APP_ID",
-	        xfbml: true,
-	        version: "v2.6"
-	      });
+var quiz = {
+	1: {
+		type: 'pickColor',
+		title: 'First Question: Pick a color', 
+		quizOptions: ['red', 'yellow'],
 
-	    };
 
-	    (function(d, s, id){
-	       var js, fjs = d.getElementsByTagName(s)[0];
-	       if (d.getElementById(id)) { return; }
-	       js = d.createElement(s); js.id = id;
-	       js.src = "//connect.facebook.net/en_US/sdk.js";
-	       fjs.parentNode.insertBefore(js, fjs);
-	    }(document, 'script', 'facebook-jssdk'));
-
-	  </script>
-			I am a quiz
-			<div class="fb-messengermessageus" 
-			  messenger_app_id="1145330698823480" 
-			  page_id="1085280641511258"
-			  color="blue"
-			  size="standard" >
-			</div>    
-	</div>`
-	
+	}
 }
 
 module.exports = quiz;
+// next, cancel
